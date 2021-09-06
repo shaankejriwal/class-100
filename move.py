b1350ec769c18ae0ab9d395f9898b941ca153c2f
shaankejriwal <70429914+shaankejriwal@users.noreply.github.com>
2021-09-06 13:06:05 +0530

    Add files via upload

---
+++ move.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+source = "newFolder"
+destination = "newfolder2"
+dest = shutil.move(source, destination)
---
+++ newfolder2/newFolder/copy.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+#path = "newfolder"
+source = "file1.txt"
+destination = "file1(copy).txt"
+dest = shutil.copy(source, destination)
